f58013ded283e41fbdefe81ffbd64c8f4f6893dd
UTUBMIR <tytybeteame@gmail.com>
2026-03-01 17:26:07 +0100

    Initial commit from UTUBMIR, added page about folder structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,36 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Unitorial',
+            locales: {
+                root: {
+                    label: 'Українська',
+                    lang: 'uk',
+                },
+            },
+            defaultLocale: 'uk',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/UTUBMIR' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    translations: { 'uk': 'Уроки' },
+                    items: [
+                        {
+                            label: 'Start',
+                            translations: { 'uk': 'Старт' },
+                            autogenerate: { directory: 'guides/start' }
+                        }
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    translations: { 'uk': 'Референси' },
+                    collapsed: true,
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
 });
